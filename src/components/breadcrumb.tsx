146f29490a819fea0bed9c1821ee92cb86df952b
Tristan Weech <twminebro@gmail.com>
2026-01-10 15:29:06 -0700

    Added JavaDocs to breadcrumb component

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { HouseIcon, GreaterThanIcon } from '../icons';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { HouseIcon, GreaterThanIcon } from "../icons";
 
 interface Props {
   location: string;
 }
 
+/**
+ * The Breadcrumb component is the history tracker of the application, allowing us to see where we've been,
+ *   allowing the forward and backward buttons to go through these views instead of a different website.
+ * The Breadcrumb trail starts at home, and has any other path follow, each path separated by a ">"
+ * @param props (location: string)
+ * @returns
+ */
 export default function Breadcrumb(props: Props) {
-  let currentPath = '';
-  const paths = props.location.split('/').map((path) => {
-    currentPath += '/' + path;
+  let currentPath = "";
+  const paths = props.location.split("/").map((path) => {
+    currentPath += "/" + path;
     return (
-      <li key={path} className="inline-flex items-center text-sm font-semibold text-gray-800 truncate" aria-current="page">
+      <li
+        key={path}
+        className="inline-flex items-center text-sm font-semibold text-gray-800 truncate"
+        aria-current="page"
+      >
         <GreaterThanIcon />
 
-        <NavLink className="flex items-center text-sm text-gray-600 hover:text-blue-600 focus:outline-none focus:text-blue-600" to={currentPath}>
+        <NavLink
+          className="flex items-center text-sm text-gray-600 hover:text-blue-600 focus:outline-none focus:text-blue-600"
+          to={currentPath}
+        >
           {path}
         </NavLink>
       </li>
@@ -23,7 +37,10 @@ export default function Breadcrumb(props: Props) {
   return (
     <ol className="flex items-center whitespace-nowrap py-2 px-4 bg-gray-300">
       <li className="inline-flex items-center">
-        <NavLink className="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600" to="/">
+        <NavLink
+          className="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600"
+          to="/"
+        >
           <span className="mx-1">
             <HouseIcon />
           </span>
