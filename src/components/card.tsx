fa8d49e0ca2e66fed974441006f07d38308825b7
Tristan Weech <twminebro@gmail.com>
2026-01-10 16:32:06 -0700

    Added JavaDocs to card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
   title: string;
@@ -6,10 +6,18 @@ interface Props {
   image: string;
 }
 
+/**
+ * Creates a standard Card item design with a Parent Card Class. This will display things such as the menu items with the image on top, the name, and the
+ *   description below it.
+ * @param props (title string, description string, image string)
+ */
 export default function Card(props: Props) {
   return (
     <div className="min-w-32 max-w-80">
-      <a className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition/[.7]" href="#">
+      <a
+        className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition/[.7]"
+        href="#"
+      >
         <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
           <img
             className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
