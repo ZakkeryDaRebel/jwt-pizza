5c56381bbd89add911a0bab75a82ccc09962bbb9
Tristan Weech <twminebro@gmail.com>
2026-02-18 13:15:24 -0700

    Added a test for updateUser where I update the email

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -69,3 +69,50 @@ test("updateUser password", async ({ page }) => {
 
   await page.getByRole("link", { name: "pd" }).click();
 });
+
+test("updateUser email", async ({ page }) => {
+  const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Register" }).click();
+  await page.getByRole("textbox", { name: "Full name" }).fill("pizza diner");
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill("diner");
+  await page.getByRole("button", { name: "Register" }).click();
+
+  await page.getByRole("link", { name: "pd" }).click();
+
+  // Edit button
+
+  await page.getByRole("button", { name: "Edit" }).click();
+  await expect(page.locator("h3")).toContainText("Edit user");
+  const newEmail = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+
+  await page.locator('input[type="email"]').click();
+  await page.locator('input[type="email"]').fill(newEmail);
+  await page.getByRole("button", { name: "Update" }).click();
+
+  await page.waitForSelector('[role="dialog"].hidden', { state: "attached" });
+
+  // Logout and See if Changes stayed
+
+  await page.getByRole("link", { name: "Logout" }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+
+  await page.getByRole("textbox", { name: "Email address" }).fill(newEmail);
+  await page.getByRole("textbox", { name: "Password" }).fill("diner");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.getByRole("link", { name: "pd" }).click();
+
+  // Logout and Make sure old email doesn't work
+
+  await page.getByRole("link", { name: "Logout" }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill("diner");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("main")).toContainText("Login");
+  await expect(page.getByText('{"code":404,"message":"')).toBeVisible();
+});
