fc357d1521d2be9240e9c1e9e06339c9cd6d86f7
Tristan Weech <twminebro@gmail.com>
2026-02-18 13:14:59 -0700

    Added a test for updateUser where I update the password

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "playwright-test-coverage";
 
-test("updateUser", async ({ page }) => {
+test("updateUser username", async ({ page }) => {
   const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
   await page.goto("http://localhost:5173/");
   await page.getByRole("link", { name: "Register" }).click();
@@ -36,4 +36,36 @@ test("updateUser", async ({ page }) => {
   await page.getByRole("link", { name: "pd" }).click();
 
   await expect(page.getByRole("main")).toContainText("pizza dinerx");
+});
+
+test("updateUser password", async ({ page }) => {
+  const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Register" }).click();
+  await page.getByRole("textbox", { name: "Full name" }).fill("pizza diner");
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill("diner");
+  await page.getByRole("button", { name: "Register" }).click();
+
+  await page.getByRole("link", { name: "pd" }).click();
+
+  // Edit button
+
+  await page.getByRole("button", { name: "Edit" }).click();
+  await expect(page.locator("h3")).toContainText("Edit user");
+  await page.locator("#password").fill("dinerx");
+  await page.getByRole("button", { name: "Update" }).click();
+
+  await page.waitForSelector('[role="dialog"].hidden', { state: "attached" });
+
+  // Logout and See if Changes stayed
+
+  await page.getByRole("link", { name: "Logout" }).click();
+  await page.getByRole("link", { name: "Login" }).click();
+
+  await page.getByRole("textbox", { name: "Email address" }).fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill("dinerx");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.getByRole("link", { name: "pd" }).click();
 });
